78f81f570eb1b8ee457d98240f254c5814b3c568
gribnoff <quinvex88@gmail.com>
2019-06-03 15:01:40 +0300

    turns + end game logic

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -15,19 +15,19 @@ public class TicTacToe {
     public static void main(String[] args) {
         initMap();
         printMap();
-/*
+
         while(true) {
             humanTurn();
             if (isEndGame(CELL_X)) {
                 break;
-            }
+            }/*
             computerTurn();
             if (isEndGame(CELL_O)) {
                 break;
-            }
+            }*/
         }
 
-        System.out.println("Игра окончена"); */
+        System.out.println("Игра окончена");
     }
 
     private static void initMap() {
@@ -51,5 +51,77 @@ public class TicTacToe {
             System.out.println();
         }
         System.out.println();
+    }
+
+    private static void humanTurn() {
+        int x, y;
+
+        do{
+            System.out.println("Введите координаты:");
+            y = scanner.nextInt() - 1;
+            x = scanner.nextInt() - 1;
+        } while(!isCellValid(x, y));
+
+        map[y][x] = CELL_X;
+    }
+
+    private static boolean isCellValid(int x, int y) {
+        boolean result = true;
+
+        if (x < 0 || x >= size || y < 0 || y >= size)
+            result = false;
+
+        if (map[y][x] != CELL_EMPTY)
+            result = false;
+
+        return result;
+    }
+
+    private static boolean isEndGame(char playerSymbol) {
+        boolean result = false;
+
+        printMap();
+
+        if (checkWin(playerSymbol)) {
+            System.out.println("Победили " + playerSymbol);
+            result = true;
+        }
+
+        if (isMapFull()) {
+            System.out.println("Ничья");
+            result = true;
+        }
+
+        return result;
+    }
+
+    private static boolean isMapFull() {
+        boolean result = true;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (map[i][j] != CELL_EMPTY)
+                    result = false;
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean checkWin(char playerSymbol) {
+        boolean result = false;
+
+        if(
+                map[0][0] == playerSymbol && map[0][1] == playerSymbol && map[0][2] == playerSymbol ||
+                map[1][0] == playerSymbol && map[1][1] == playerSymbol && map[1][2] == playerSymbol ||
+                map[2][0] == playerSymbol && map[2][1] == playerSymbol && map[2][2] == playerSymbol ||
+                map[0][0] == playerSymbol && map[1][0] == playerSymbol && map[2][0] == playerSymbol ||
+                map[0][1] == playerSymbol && map[1][1] == playerSymbol && map[2][1] == playerSymbol ||
+                map[0][2] == playerSymbol && map[1][2] == playerSymbol && map[2][2] == playerSymbol ||
+                map[0][0] == playerSymbol && map[1][1] == playerSymbol && map[2][2] == playerSymbol ||
+                map[2][0] == playerSymbol && map[1][1] == playerSymbol && map[0][2] == playerSymbol) {
+            result = true;
+        }
+        return result;
     }
 }
